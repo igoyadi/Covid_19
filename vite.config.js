624593bd968bff968eefd8a080126e3de3dc8d14
Ishika Goyadi <ishikag@Ishikas-MacBook-Air.local>
2025-06-17 20:23:41 +0530

    added host

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',                 
     port: parseInt(process.env.PORT) || 5173,
+    allowedHosts: ['covid-19-wuzy.onrender.com'],
   },
 })
